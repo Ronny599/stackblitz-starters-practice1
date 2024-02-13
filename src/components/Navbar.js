f60be6aaf0a714ea6b4ec3c9a259da27b8f0ec2b
Ronny599 <kumarronit599@gmail.com>
2024-02-13 12:38:23 +0530

    Structural Formation_1

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export default function Navbar(){
+    return(
+        <>
+        <nav class="navbar bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+              Bootstrap
+            </a>
+          </div>
+        </nav>
+        </>
+    );
+}
