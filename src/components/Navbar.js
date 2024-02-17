0dc933c049d84a0d869e56a23d913c5b164e0673
Ronny599 <kumarronit599@gmail.com>
2024-02-17 20:19:07 +0530

    image link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(){
         <nav class="navbar bg-body-tertiary">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">
-              <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+              <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1708128000&semt=sph" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
               Title
             </a>
           </div>
