8aed3db55f70f7f5bfbb4277f4ca6b6ba9605e4c
Ronny599 <kumarronit599@gmail.com>
2024-02-17 19:55:20 +0530

    package updation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ export default function Navbar(){
         <nav class="navbar bg-body-tertiary">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">
-              <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
-              Bootstrap
+              <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+              Title
             </a>
           </div>
         </nav>
