deeab13855bab6603899c400e418640aea7368bd
Ronny599 <kumarronit599@gmail.com>
2024-02-17 20:28:24 +0530

    error removal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,7 @@ export default function Navbar(){
         <nav class="navbar bg-body-tertiary">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">
-              <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1708128000&semt=sph" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
-              Title
+              <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
             </a>
           </div>
         </nav>
